de21664e9e8e1d8b45af52a1cdaaf0bbbc5d60f3
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2019-11-11 14:58:16 -0500

    Wrote deterministic k method for generating k

--- ecc.py
+++ ecc.py
@@ -451,4 +451,25 @@ class PrivateKey:
         if s > N/2:
             s = N - s
         return Signature(r, s)
+    
+    def deterministic_k(self, z):
+        # Returns a deterministic k, such that the probability of repeating a k is much lower.
+        k = b'\x00' * 32
+        v = b'\x01' * 32
+        if z > N:
+            z -= N
+        z_bytes = z.to_bytes(32, 'big')
+        secret_bytes = self.secret.to_bytes(32, 'big')
+        s256 = hashlib.sha256
+        k = hmac.new(k, v + b'\x00' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        k = hmac.new(k, v + b'\x01' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        while True:
+            v = hmac.new(k, v, s256).digest()
+            candidate = int.from_bytes(v, 'big')
+            if candidate >= 1 and candidate < N:
+                return candidate
+            k = hmac.new(k, v + b'\x00', s256).digest()
+            v = hmac.new(k, v, s256).digest()
 
