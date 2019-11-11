93903ee4238c9cbac5997055c6f21b2bae81562b
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2019-11-11 14:30:05 -0500

    Wrote add test for ECCTest class

--- ecc.py
+++ ecc.py
@@ -1,5 +1,6 @@
 from random import randint
 from unittest import TestCase
+from helper import hash256
 
 import hashlib
 import hmac
@@ -260,12 +261,12 @@ class ECCTest(TestCase):
         for x_raw, y_raw in valid_points:
             x = FieldElement(x_raw, prime)
             y = FieldElement(y_raw, prime)
-            Point(x, y, a, b)  # <1>
+            Point(x, y, a, b)  
         for x_raw, y_raw in invalid_points:
             x = FieldElement(x_raw, prime)
             y = FieldElement(y_raw, prime)
             with self.assertRaises(ValueError):
-                Point(x, y, a, b)  # <1>
+                Point(x, y, a, b) 
 
     def test_add(self):
         # tests the following additions on curve y^2=x^3-7 over F_223:
@@ -287,7 +288,18 @@ class ECCTest(TestCase):
         # initialize x's and y's as FieldElements
         # create p1, p2 and p3 as Points
         # check p1+p2==p3
-        raise NotImplementedError
+        for x1_raw, y1_raw, x2_raw, y2_raw, x3_raw, y3_raw in additions:
+            x1 = FieldElement(x1_raw, prime)
+            y1 = FieldElement(y1_raw, prime)
+            x2 = FieldElement(x2_raw, prime)
+            y2 = FieldElement(y2_raw, prime)
+            x3 = FieldElement(x3_raw, prime)
+            y3 = FieldElement(y3_raw, prime)
+            p1 = Point(x1, y1, a, b)
+            p2 = Point(x2, y2, a, b)
+            p3 = Point(x3, y3, a, b)
+            self.assertEqual(p1 + p2, p3)
+
 
     def test_rmul(self):
         # tests the following scalar multiplications
@@ -332,9 +344,6 @@ A = 0
 B = 7
 P = 2**256 - 2**32 - 977
 N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
-G = S256Point(
-    0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
-    0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
 
 class S256Field(FieldElement):
     
@@ -373,6 +382,45 @@ class S256Point(Point):
 
         return R.x.num == sig.r
 
+G = S256Point(
+    0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+    0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
+
+class S256Test(TestCase):
+
+    def test_order(self):
+        point = N * G
+        self.assertIsNone(point.x)
+
+    def test_pubpoint(self):
+        # write a test that tests the public point for the following
+        points = (
+            # secret, x, y
+            (7, 0x5cbdf0646e5db4eaa398f365f2ea7a0e3d419b7e0330e39ce92bddedcac4f9bc, 0x6aebca40ba255960a3178d6d861a54dba813d0b813fde7b5a5082628087264da),
+            (1485, 0xc982196a7466fbbbb0e27a940b6af926c1a74d5ad07128c82824a11b5398afda, 0x7a91f9eae64438afb9ce6448a1c133db2d8fb9254e4546b6f001637d50901f55),
+            (2**128, 0x8f68b9d2f63b5f339239c1ad981f162ee88c5678723ea3351b7b444c9ec4c0da, 0x662a9f2dba063986de1d90c2b6be215dbbea2cfe95510bfdf23cbf79501fff82),
+            (2**240 + 2**31, 0x9577ff57c8234558f293df502ca4f09cbc65a6572c842b39b366f21717945116, 0x10b49c67fa9365ad7b90dab070be339a1daf9052373ec30ffae4f72d5e66d053),
+        )
+
+        # iterate over points
+        for secret, x, y in points:
+            # initialize the secp256k1 point (S256Point)
+            point = S256Point(x, y)
+            # check that the secret*G is the same as the point
+            self.assertEqual(secret * G, point)
+
+    def test_verify(self):
+        point = S256Point(
+            0x887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c,
+            0x61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34)
+        z = 0xec208baa0fc1c19f708a9ca96fdeff3ac3f230bb4a7ba4aede4942ad003c0f60
+        r = 0xac8d1c87e51d0d441be8b3dd5b05c8795b48875dffe00b7ffcfac23010d3a395
+        s = 0x68342ceff8935ededd102dd876ffd6ba72d6a427a3edb13d26eb0781cb423c4
+        self.assertTrue(point.verify(z, Signature(r, s)))
+        z = 0x7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d
+        r = 0xeff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c
+        s = 0xc7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6
+        self.assertTrue(point.verify(z, Signature(r, s)))
 
 class Signature:
 
@@ -382,6 +430,3 @@ class Signature:
 
     def __repr__(self):
         return f"Signature ({self.r}, {self.s})"
-
-e = 12345
-z = int.from_bytes(hash256("Programming Bitcoin!"), "big")
