22ac04f0946b43855c36604d2ccf6dd1dc7b508a
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2019-11-16 11:46:59 -0500

    Wrote op_sha1 function and test for it

--- op.py
+++ op.py
@@ -729,6 +729,15 @@ def op_ripemd160(stack):
     stack.append(hashlib.new('ripemd160', elem).digest())
     return True
 
+# Consumes top element of the stack, performs a sha1 operation on it and pushes the hashed element
+# into the stack.
+def op_sha1(stack):
+    if len(stack) < 1:
+        return False
+    elem = stack.pop()
+    stack.append(hashlib.new('sha1', elem).digest())
+    return True
+
 
 class TestOp(TestCase):
 
@@ -967,6 +976,11 @@ class TestOp(TestCase):
         stack = [encode_num(1)]
         op_ripemd160(stack)
         self.assertEqual(stack, [hashlib.new('ripemd160', encode_num(1)).digest()])
+    
+    def test_op_sha1(self):
+        stack = [encode_num(1)]
+        op_sha1(stack)
+        self.assertEqual(stack, [hashlib.new('sha1', encode_num(1)).digest()])
 
 
 OP_CODE_FUNCTIONS = {
@@ -1036,7 +1050,7 @@ OP_CODE_FUNCTIONS = {
     164: op_max,
     165: op_within,
     166: op_ripemd160,
-    # 167: op_sha1,
+    167: op_sha1,
     # 168: op_sha256,
     169: op_hash160,
     170: op_hash256,
