8d8865e9a2b4056265d58566bd17f8ee9a6a3d00
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2019-11-21 13:54:16 -0500

    Wrote init and repr methods for NetworkEnvelope class

---
+++ network.py
@@ -0,0 +1,36 @@
+import socket
+import time
+
+from io import BytesIO
+from random import randint
+from unittest import TestCase
+
+from block import Block
+from helper import (
+    hash256,
+    encode_varint,
+    int_to_little_endian,
+    little_endian_to_int,
+    read_varint,
+)
+
+TX_DATA_TYPE = 1
+BLOCK_DATA_TYPE = 2
+FILTERED_BLOCK_DATA_TYPE = 3
+COMPACT_BLOCK_DATA_TYPE = 4
+
+NETWORK_MAGIC = b'\xf9\xbe\xb4\xd9'
+TESTNET_NETWORK_MAGIC = b'\x0b\x11\x09\x07'
+
+class NetworkEnvelope:
+
+    def __init__(self, command, payload, testnet=False):
+        self.command = command
+        self.payload = payload
+        if testnet:
+            self.magic = TESTNET_NETWORK_MAGIC
+        else:
+            self.magic = NETWORK_MAGIC
+    
+    def __repr__(self):
+        return '{}: {}'.format(self.command.decode('ascii'), self.payload.hex())
